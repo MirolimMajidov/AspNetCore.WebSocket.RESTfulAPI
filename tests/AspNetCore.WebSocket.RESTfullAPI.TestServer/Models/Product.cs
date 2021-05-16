0c452a069c98efc3c5371d7f832363a8335e2874
Mirolim Majidov <majidovmirolimjon@gmail.com>
2021-05-16 19:26:06 +0500

    released new version of NuGet and used it

---
+++ tests/AspNetCore.WebSocket.RESTfullAPI.TestServer/Models/Product.cs
@@ -0,0 +1,9 @@
+namespace AspNetCore.WebSocket.RESTfullAPI.TestServer
+{
+    public class Product
+    {
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+    }
+}
