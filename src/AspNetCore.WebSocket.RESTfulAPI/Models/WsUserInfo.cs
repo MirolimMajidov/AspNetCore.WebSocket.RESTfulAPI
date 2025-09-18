17824fb12c169c8d6e6907f1abf527eeaab6d45f
Mirolim Majidov <mirolimmajidov@outlook.com>
2025-09-18 15:11:15 +0500

    Add summary comments for Id and Name properties in WsUserInfo

--- src/AspNetCore.WebSocket.RESTfulAPI/Models/WsUserInfo.cs
+++ src/AspNetCore.WebSocket.RESTfulAPI/Models/WsUserInfo.cs
@@ -4,7 +4,13 @@ namespace AspNetCore.WebSocket.RESTfulAPI.Models;
 
 public class WsUserInfo : Disposable
 {
+    /// <summary>
+    /// The user's Id
+    /// </summary>
     public required Guid Id { get; init; }
 
+    /// <summary>
+    /// The user's Name
+    /// </summary>
     public required string Name { get; set; }
 }
